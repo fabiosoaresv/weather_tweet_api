02c17568d555b45261facc2473b63761ef4e7b8f
Fabio Soares <soaresventurellifabio@gmail.com>
2024-01-28 20:18:02 -0300

    feat: update config to add dotenv

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,21 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
-require "active_model/railtie"
-require "active_job/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "rails/test_unit/railtie"
+require 'rails'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'rails/test_unit/railtie'
+require 'dotenv'
 
 Bundler.require(*Rails.groups)
+Dotenv.load
 
 module WeatherTweetApi
   class Application < Rails::Application
     config.load_defaults 7.1
-    config.autoload_lib(ignore: %w(assets tasks))
     config.api_only = true
     config.autoload_paths += %W(#{config.root}/app/services)
   end
